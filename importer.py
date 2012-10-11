b7182e3b723b7b388c13baaf844e918a898929b8
Mike Boers <mboers@keypics.com>
2012-10-11 09:34:54 -0700

    Import some FamFamFam "silk" icons
    See: http://www.famfamfam.com/lab/icons/silk/

--- importer.py
+++ importer.py
@@ -45,7 +45,13 @@ def delete_cache(node):
 
 
 def silk(name):
-    return '/home/mboers/Documents/icons/silk/icons/%s.png' % name
+    # __file__ = "$KS_TOOLS/key_base/3d/maya/python/geocache/importer.py"
+    # icons = "$KS_TOOLS/key_base/art/icons"
+    icons = os.path.abspath(os.path.join(__file__,
+        '..', '..', '..', '..', '..',
+        'art', 'icons', 'silk'))
+    return os.path.join(icons, name + '.png')
+
 
 def silk_icon(name, size=16):
     icon = QtGui.QIcon(silk(name))
