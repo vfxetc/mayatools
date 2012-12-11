45d734cb495e7f61c5cbbac2958e220868033a9d
Mike Boers <mboers@keypics.com>
2012-12-11 15:46:35 -0800

    Add setup.py for RTD

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='mayatools',
+    version='0.1-dev',
+    description='Collection of general tools and utilities for working in and with Maya.',
+    url='https://github.com/westernx/mayatools',
+    
+    packages=['mayatools'],
+    
+    author='Mike Boers',
+    author_email='mayatools@mikeboers.com',
+    license='BSD-3',
+    
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
