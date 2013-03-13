be25f76562fabe4002806d8f9771386f9a9bc9b4
Mike Boers <mboers@keypics.com>
2013-03-13 14:32:01 -0700

    Binary: pprint decodes basic types

--- mayatools/binary.py
+++ mayatools/binary.py
@@ -1,4 +1,5 @@
 import array
+import functools
 import itertools
 import struct
 import string
@@ -7,21 +8,131 @@ import string
 _is_printable = set(string.printable).difference(string.whitespace).__contains__
 
 
+class Encoder(object):
+
+    def split(self, encoded, size_hint):
+        for i in xrange(0, len(encoded), size_hint):
+            yield encoded[i:i + size_hint]
+
+    def repr_chunk(self, chunk):
+        return ''.join(c if _is_printable(c) else '.' for c in chunk)
+
+
+class StructEncoder(Encoder):
+
+    def __init__(self, format_char):
+        self.format_char = format_char
+        self.size = struct.Struct('>' + format_char).size
+
+    def split(self, encoded, size_hint):
+        size_hint = size_hint - (size_hint % self.size)
+        return super(StructEncoder, self).split(encoded, size_hint)
+
+    def unpack(self, encoded):
+        count, rem = divmod(len(encoded), self.size)
+        if rem:
+            raise ValueError('encoded length %d is not multiple of %d; %d remains' % (len(encoded), self.size, rem))
+        return struct.unpack('>%d%s' % (count, self.format_char), encoded)
+
+    def repr_chunk(self, encoded):
+        return ' '.join(repr(x) for x in self.unpack(encoded))
+
+
+class StringEncoder(Encoder):
+
+    def split(self, encoded, size_hint):
+        return encoded.rstrip('\0').split('\0')
+
+    def repr_chunk(self, chunk):
+        return repr(chunk)
+
+
+# Map encoding names to the object which exposes the Encoder interface.
+encoders = {}
+
+
+def register_encoder(names, encoder=None):
+    """Decorator to register encoders."""
+    if isinstance(names, basestring):
+        names = [names]
+    if encoder is None:
+        return functools.partial(register_encoder, names)
+    else:
+        for name in names:
+            encoders[name] = encoder
+        return encoder
+
+
+register_encoder('float', StructEncoder('f'))
+register_encoder('uint', StructEncoder('L'))
+register_encoder('string', StringEncoder())
+
+
+# Map tag names to the name of an encoding.
+tag_encoding = {
+    
+    # Maya headers.
+    'VERS': 'string', # app version
+    'UVER': 'string',
+    'MADE': 'string',
+    'CHNG': 'string', # timestamp
+    'ICON': 'string',
+    'INFO': 'string',
+    'OBJN': 'string',
+    'INCL': 'string',
+    'LUNI': 'string', # linear unit
+    'TUNI': 'string', # time unit
+    'AUNI': 'string', # angle unit
+    'FINF': 'string', # file info
+
+    # Generic.
+    'SIZE': 'uint',
+
+    # DAG
+    'CREA': 'string', # create node
+    'STR ': 'string', # string attribute
+
+    # Cache headers.
+    'VRSN': 'string', # cache version
+    'STIM': 'uint',   # cache start time
+    'ETIM': 'uint',   # cache end time
+
+    # Cache channels.
+    'CHNM': 'string', # channel name
+
+    # Cache data.
+    'FBCA': 'float',  # floating cache array
+}
+
+
+def get_encoder(tag):
+    encoding = tag_encoding.get(tag, 'raw')
+    return encoders.get(encoding) or Encoder()
+
+
 def hexdump(*args, **kwargs):
     return ''.join(_hexdump(*args, **kwargs))
 
-def _hexdump(raw, initial_offset=0, chunk=4, line=16, indent=''):
-    chunk2 = chunk * 2
-    line2 = line * 2
-    for i in xrange(0, len(raw), line):
+def _hexdump(raw, initial_offset=0, chunk=4, line=16, indent='', tag=None):
+
+    chunk2 = 2 * chunk
+    line2 = 2 * line
+    encoder = get_encoder(tag)
+    offset = initial_offset
+
+    for encoded_chunk in encoder.split(raw, line):
+
         yield indent
-        yield '%04x: ' % (i + initial_offset)
-        raw_line = raw[i:i + line]
-        encoded = raw_line.encode('hex')
-        encoded += ' ' * (line2 - len(encoded))
-        for j in xrange(0, line2, chunk2):
-            yield '%s ' % encoded[j:j + chunk2]
-        yield ''.join(c if _is_printable(c) else '.' for c in raw_line)
+        yield '%04x: ' % offset
+        offset += len(encoded_chunk)
+
+        # Encode the chunk to hex, pad it, and chunk it further.
+        hex_chunk = encoded_chunk.encode('hex')
+        hex_chunk += ' ' * (line2 - len(hex_chunk))
+        for i in xrange(0, len(hex_chunk), chunk2):
+            yield '%s ' % hex_chunk[i:i + chunk2]
+
+        yield encoder.repr_chunk(encoded_chunk)
         yield '\n'
 
 
@@ -125,8 +236,13 @@ class Chunk(object):
             setattr(self, k, v)
 
     def pprint(self, _indent):
-        print _indent * '    ' + ('%s; %d bytes' % (self.tag, len(self.data)))
-        print hexdump(self.data, self.offset, indent=(_indent + 1) * '    ').rstrip()
+        encoding = tag_encoding.get(self.tag)
+        if encoding:
+            header = '%d bytes as %s(s)' % (len(self.data), encoding)
+        else:
+            header = '%d raw bytes' % len(self.data)
+        print _indent * '    ' + ('%s; %s' % (self.tag, header))
+        print hexdump(self.data, self.offset, tag=self.tag, indent=(_indent + 1) * '    ').rstrip()
 
     def __repr__(self):
         return '<%s %s; %d bytes>' % (self.__class__.__name__, self.tag, len(self.data))
@@ -240,8 +356,28 @@ class Parser(Node):
 
 if __name__ == '__main__':
     import sys
-    
-    parser = Parser(open(sys.argv[1]))
-    parser.parse_all()
-    parser.pprint()
+    from optparse import OptionParser
+
+    opt_parser = OptionParser()
+    opt_parser.add_option('-t', '--type', action='append', default=[])
+    opt_parser.add_option('-n', '--no-types', action='store_true')
+    opts, args = opt_parser.parse_args()
+
+    if opts.no_types:
+        tag_encoding.clear()
+
+    for type_spec in opts.type:
+        type_spec = type_spec.split(':')
+        if len(type_spec) == 1:
+            tag_encoding.pop(type_spec[0])
+        elif len(type_spec) == 2:
+            tag_encoding[type_spec[0]] = type_spec[1]
+        else:
+            raise ValueError('type spec should look like NAME:type')
+
+
+    for arg in args:
+        parser = Parser(open(arg))
+        parser.parse_all()
+        parser.pprint()
 
