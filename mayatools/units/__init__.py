e01330f0e433d86229185cd1122ad638a687d628
Mike Boers <github@mikeboers.com>
2016-10-07 12:07:40 -0400

    Units: Refactor fps code to adapt to future unnamed frame rates

---
+++ mayatools/units/__init__.py
@@ -0,0 +1,1 @@
+from .core import get_fps, set_fps
--- mayatools/units/core.py
+++ mayatools/units/core.py
@@ -1,8 +1,12 @@
+import re
+
 from maya import cmds 
 
 
+# These are all of the named framerates. The rest of them look
+# like "100fps".
 unit_to_fps = {
-	
+    'sec': 1,
     'game': 15, 
     'film': 24, 
     'pal': 25, 
@@ -10,64 +14,63 @@ unit_to_fps = {
     'show': 48,  
     'palf': 50, 
     'ntscf': 60,
-    '2fps': 2,
-    '3fps': 3,
-    '4fps': 4,
-    '5fps': 5,
-    '6fps': 6,
-    '8fps': 8,
-    '10fps': 10,
-    '12fps': 12,
-    '16fps': 16,
-    '20fps': 20,
-    '40fps': 40,
-    '75fps': 75,
-    '80fps': 80,
-    '100fps': 100,
-    '120fps': 120,
-    '125fps': 125,
-    '150fps': 150,
-    '200fps': 200,
-    '240fps': 240,
-    '250fps': 250,
-    '300fps': 300,
-    '375fps': 375,
-    '400fps': 400,
-    '500fps': 500,
-    '600fps': 600,
-    '750fps': 750,
-    '1200fps': 1200,
-    '1500fps': 1500,
-    '2000fps': 2000,
-    '3000fps': 3000,
-    '6000fps': 6000
+    'millisec': 1000,
+    # NOTE: We don't support 'min' or 'hour' here because we return integers.
 }
 
-fps_to_unit = {
-    v: k
-    for k, v
-    in unit_to_fps.iteritems()
-}
+fps_to_unit = {v: k for k, v in unit_to_fps.iteritems()}
+
+valid_fpses = frozenset((
+
+    # The named ones above.
+    15, 24, 25, 30, 48, 50, 60, 
+
+    # The rest of the known valid FPSes as of Maya 2016.
+    2, 3, 4, 5, 6, 8, 10, 12, 16, 20, 40, 75, 80, 100, 120, 125, 150, 200,
+    240, 250, 300, 375, 400, 500, 600, 750, 1200, 1500, 2000, 3000, 6000,
+
+))
 
 
 def get_fps():
     '''
-    Gets framerate and returns the unit as integer. 
+    Get current framerate as an integer.
+
+    ::
+        >>> units.get_fps()
+        24
+
     '''
+
     unit = cmds.currentUnit(q=True, time=True)
     try: 
         return unit_to_fps[unit]
-    except KeyError: 
-        raise ValueError("Unknown framerate. %s" % unit)
+    except KeyError:
+        pass
+
+    m = re.match(r'(\d+)fps', unit)
+    if m:
+        return int(m.group(1))
+
+    raise ValueError("Unknown Maya time unit %r" % unit)
 
 
 def set_fps(fps):
     '''
-    Takes an int as an argument and sets the framerate. 
+    Set current framerate as an integer.
+
+    :param int fps: The framerate to set.
+
+    ::
+        >>> units.set_fps(12)
+        >>> units.get_fps()
+        12
+
     '''
-    try: 
-        unit = fps_to_unit[fps]
-    except KeyError: 
-        raise ValueError("Bad framerate. %s" % fps)
-    cmds.currentUnit(time=unit)
+
+    unit = fps_to_unit.get(fps) or ('%dfps' % fps)
+    try:
+        cmds.currentUnit(time=unit)
+    except ValueError:
+        raise ValueError("Unsupported framerate %s" % fps)
 
