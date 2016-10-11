ff703f0f1714daae2c8117cf1f37e16b2d77ae6b
Elaine Wong <elainew@markmedia.co>
2016-10-11 18:26:44 -0400

    ui now takes its framerate list from core.py

--- mayatools/units/ui.py
+++ mayatools/units/ui.py
@@ -1,49 +1,23 @@
 from uitools.qt import Qt, QtCore, QtGui
 import mayatools.units as units
 
+dialog = None
+unit_to_fps = {}
+temp_list = []
 
-unit_to_fps = {
-    'sec (1 fps)': 1,
-    'game (15 fps)': 15, 
-    'film (24 fps)': 24, 
-    'pal (25 fps)': 25, 
-    'ntsc (30 fps)': 30, 
-    'show (48 fps)': 48,  
-    'palf (50 fps)': 50, 
-    'ntscf (60 fps)': 60,
-    'millisec (1000 fps)': 1000,
-       '2 fps': 2, 
-       '3 fps ': 3, 
-       '4 fps': 4, 
-       '5 fps': 5, 
-       '6 fps': 6, 
-       '8 fps': 8, 
-       '10 fps': 10, 
-       '12 fps': 12, 
-       '16 fps': 16, 
-       '20 fps': 20, 
-       '40 fps': 40, 
-       '75 fps': 75,
-       '80 fps': 80, 
-       '100 fps': 100, 
-       '120 fps': 120, 
-       '125 fps': 125, 
-       '150 fps': 150, 
-       '200 fps': 200,
-       '240 fps': 240, 
-       '250 fps': 250, 
-       '300 fps': 300, 
-       '375 fps': 375, 
-       '400 fps': 400, 
-       '500 fps': 500, 
-       '600 fps': 600, 
-       '750 fps': 750, 
-       '1200 fps': 1200, 
-       '1500 fps': 1500, 
-       '2000 fps': 2000, 
-       '3000 fps': 3000,
-       '6000 fps': 6000
-}
+temp_list = list(units.core.valid_fpses)
+
+for item in units.core.fps_to_unit:
+    if item in temp_list:
+        temp_list.remove(item)
+
+for item in temp_list:
+    name = str(item) + " fps"
+    unit_to_fps[name] = item
+
+for item in units.core.unit_to_fps:
+    name = item + " " + str(units.core.unit_to_fps[item]) + " fps"
+    unit_to_fps[name] = units.core.unit_to_fps[item]
 
 
 
@@ -88,7 +62,7 @@ class MyDialog(QtGui.QDialog):
         print units.core.get_fps()
         self.close()
 
-dialog = None
+
 
 def run():
     
